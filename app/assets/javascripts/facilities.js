0034687c68587bde34940693709eb3a903d93235
Chau Hong Linh <chauhonglinh@gmail.com>
2016-06-06 00:45:33 -0700

    Rearrange DataTables sorting.

--- app/assets/javascripts/facilities.js
+++ app/assets/javascripts/facilities.js
@@ -8,14 +8,21 @@ ready = function() {
         },
         {
             targets: [ 1 ],
-            orderData: [ 0, 1 ]
-        }, {
-            targets: [ 2 ],
             orderData: [ 1, 0 ]
-        }, {
-            targets: [ 5 ],
-            orderData: [ 4, 0 ]
-        } 
+        }, 
+        {
+            targets: [ 2 ],
+            orderData: [ 2, 0 ]
+        }, 
+        {
+            targets: [ 3 ],
+            orderData: [ 3, 0 ]
+        },
+        {
+            targets: [ 12 ],
+            orderData: [ 12, 0, 1 ]
+        }
+
       ]
   }); // data table
 
